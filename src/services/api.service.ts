b5e8b737d69644b5aaab0569e24463db88ea3c69
marchenko_242 <marchenko.ar06@gmail.com>
2025-10-07 00:06:18 +0300

    resipes-miniproject

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,31 +1,68 @@
-import axios from "axios";
-import {IUser} from "../models/IUser";
-import {IPost} from "../models/IPost";
-import {IComment} from "../models/IComment";
+import {IDish} from "../models/IDish";
+import {IDishByID} from "../models/IDishByID";
 
-let axiosInstance = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com",
-    headers: {}
-});
+export const getDataFromDB = {
+    breakfast: {
+        getBreakfast: async (): Promise<IDish[]> => {
+            const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=breakfast&number=500&apiKey=8ff8c946b881419d97b01876077465f6`);
+            const data = await response.json();
+            return data.results || [];
+        },
+        getBreakfastByID: async (id: number): Promise<IDishByID> => {
+            const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=8ff8c946b881419d97b01876077465f6`);
+            const data = await response.json();
+            return data;
+        }
+    },
+    lunch: {
+        getLunches: async () : Promise<IDish[]> => {
+            const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?query=lunch&number=500&apiKey=8ff8c946b881419d97b01876077465f6");
+            const data = await response.json();
+            return data.results || [];
+        },
+        getLunchesByID: async (id: number): Promise<IDishByID> => {
+            const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=8ff8c946b881419d97b01876077465f6`);
+            const data = await response.json();
+            return data;
+        }
+    },
+    dinners: {
+        getDinners: async (): Promise<IDish[]> => {
+            const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?query=dinner&apiKey=8ff8c946b881419d97b01876077465f6");
+            const data = await response.json();
+            return data.results || [];
+        },
+        getDinnersByID: async (id: number): Promise<IDishByID> => {
+            const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=8ff8c946b881419d97b01876077465f6`);
+            const data = await response.json();
+            return data;
+        }
+    },
+    desserts: {
+        getDessert: async (): Promise<IDish[]> => {
+            const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?query=dessert&apiKey=8ff8c946b881419d97b01876077465f6");
+            const data = await response.json();
+            return data.results || [];
+        },
+        getDessertByID: async (id: number): Promise<IDishByID> => {
+            const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=8ff8c946b881419d97b01876077465f6`);
+            const data = await response.json();
+            return data;
+        }
+    },
+    drinks: {
+        getDrinks: async (): Promise<IDish[]> => {
+            const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?query=drink&apiKey=8ff8c946b881419d97b01876077465f6");
+            const data = await response.json();
+            return data.results || [];
+        },
+        getDrinkByID: async (id: number): Promise<IDishByID> => {
+            const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=8ff8c946b881419d97b01876077465f6`);
+            const data = response.json();
+            return data;
+        }
+    }
 
-export let getPosts = async (): Promise<IPost[]> => {
-    let getData = await axiosInstance.get<IPost[]>("/posts");
-    return getData.data
-}
-
-export let getUsers = async (): Promise<IUser[]> => {
-    let getData = await axiosInstance.get<IUser[]>("/users");
-    return getData.data
-}
-
-export let getComments = async (): Promise<IComment[]> => {
-    let getData = await axiosInstance.get<IComment[]>("/comments");
-    return getData.data
-}
-
-export let getCommentsByPosts = async (id: string) => {
-    let response = await axiosInstance.get<IComment[]>('comments?postId=' + id)
-    return response.data
 }
 
 
